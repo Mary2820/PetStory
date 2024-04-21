dd3775c5dd6cdf6bba88a8640687b69dfb202abb
Mary2820 <mdedul2820@gmail.com>
2024-04-21 16:54:33 +0200

    Created: project with Rest Assured.

---
+++ src/main/java/petStory/Category.java
@@ -0,0 +1,19 @@
+package petStory;
+
+public class Category {
+    public String id;
+    public String name;
+
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
