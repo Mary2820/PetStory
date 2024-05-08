f236fe259d2fe8c581d9d05fbbb68c6bcfb75ef9
DESKTOP-A26UO4C\mdedu <mdedul2820@gmail.com>
2024-05-08 15:38:50 +0200

    2

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,29 +1,29 @@
-//pipeline {
-//    agent any
-//
-//    stages {
-//        stage('Checkout') {
-//            steps {
-//                checkout scm
-//            }
-//        }
-//
-//        stage('Build') {
-//            steps {
-//                sh 'mvn clean install'
-//            }
-//        }
-//
-//        stage('Tests') {
-//            steps {
-//                sh 'mvn verify'
-//            }
-//        }
-//
-//        stage('Allure Report') {
-//            steps {
-//                allureReport(reportDir: 'target/allure-reports')
-//            }
-//        }
-//    }
-//}
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                echo "checkoting ..."
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo "building ..."
+            }
+        }
+
+        stage('Tests') {
+            steps {
+                echo "testing ..."
+            }
+        }
+
+        stage('Allure Report') {
+            steps {
+                echo "repoting ..."
+            }
+        }
+    }
+}
